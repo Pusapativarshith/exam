847aa2394e2d4bfc348e620866857105a166794c
Pusapativarshith <134836882+Pusapativarshith@users.noreply.github.com>
2024-11-07 19:25:27 +0530

    Create json2html.js
    code

---
+++ json2html.js
@@ -0,0 +1,19 @@
+export default function json2html(data) {
+    const headers = ["Name", "Age", "Gender"];
+    const table = `
+      <table data-user="pusapati.varshithreddy@gmail.com">
+        <thead>
+          <tr>${headers.map(header => <th>${header}</th>).join('')}</tr>
+        </thead>
+        <tbody>
+          ${data.map(row => `
+            <tr>
+              ${headers.map(header => <td>${row[header] || ""}</td>).join('')}
+            </tr>
+          `).join('')}
+        </tbody>
+      </table>
+    `;
+    
+    return table;
+  }
